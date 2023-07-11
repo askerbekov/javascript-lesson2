783c80c4e8bc83a7eff9ad1d3c325359ec261b53
askerbekov <askerbekov.mederbek@gmail.com>
2023-07-11 15:55:55 +0600

    commit by js lesson

---
+++ script.js
@@ -0,0 +1,79 @@
+// let a = 'Mederbek'
+// let b = 'Askerbekov'
+// let c = a.slice(-3).toUpperCase(a.slice(0))
+// let d = a.slice(0,-3)
+//
+// let e = b.slice(-3).toUpperCase(b.slice(0))
+// let f = b.slice(0,-3)
+//
+// console.log(d + c + " " + f + e)
+
+// let a = 'hello'
+// let b = 'Ivan'
+// let c = a.substring(0,1).toUpperCase()
+// let d = a.slice(1)
+//
+// console.log(c + d + " " + b)
+
+// let a = 'lorem ipsum dolor'
+// let b = a.length
+//
+// console.log(b)
+
+// let a = 'lorem ipsum dolor sit amet'
+// let b = a.substring(17,22)
+// let c = a.slice(0,17)
+// let d = a.slice(22,26)
+//
+// console.log(b + c + d)
+
+// let a = "children"
+// let b = a.slice(0 ,-1).toUpperCase()
+// let c = a.slice(a.length - 1)
+//
+// console.log(b + c)
+
+// let a = 'boss'
+// let b = a.slice(1,2)
+// let c = a.slice(0,1)
+// let d =a.slice(2,4)
+// console.log(c + d + b)
+
+// let a = 'sunday'
+// console.log(a.indexOf('d'))
+
+
+//  let a = 'hello'
+// console.log(a.slice(0,2) + "kso")
+
+// let a = 'user'
+// let b = 'new'
+// let c = a.substring(0,2)
+//
+// console.log(a + " " + c)
+
+//  let a = 'monday'
+//  let b = 'day'
+//  let c = a.slice(0,3)
+//
+// console.log(b + b + c)
+
+// let a = 'HELLO'
+// let b = a.slice(0,1).toLowerCase()
+// let c = a.slice(a.length -1).toLowerCase()
+// let d = a.slice(1,4)
+//
+// console.log(b + d + c)
+
+// let a = 5
+// let b = '400'
+//
+// console.log(b.replace('0', a))
+
+let a = 'monkey'
+let b = 'banana'
+
+let c = a.slice(0,1)
+let d = a.slice(3,4)
+let f = b.slice(1 ,4)
+console.log(c + f + d)
